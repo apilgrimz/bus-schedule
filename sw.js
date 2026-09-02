14c6cb8009533144cc613dc0df94c618ef0196b9
apilgrimz <apilgrim@naver.com>
2026-09-02 10:18:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
-// 캐시 사용 안 함 — 항상 최신 파일 로드
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(
-  caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
-));
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request, {cache: 'no-store'}).catch(() => new Response('')));
-});
+self.addEventListener('fetch', function(e) {});
